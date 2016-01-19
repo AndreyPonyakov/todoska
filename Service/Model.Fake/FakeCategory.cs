940fcb55eabaef53999bcfd3acd486b3826c1096
Ansi <the-ansi@yandex.ru>
2016-01-19 23:53:57 +0300

    Fixfed commit

--- Service/Model.Fake/FakeCategory.cs
+++ Service/Model.Fake/FakeCategory.cs
@@ -4,13 +4,15 @@ using Todo.Service.Model.Interface;
 
 namespace Todo.Service.Model.Fake
 {
+    /// <summary>
+    /// Fake class of ICategory
+    /// </summary>
     public class FakeCategory : ICategory
     {
         public int Id { get; }
         public string Name { get; set; }
         public Color Color { get; set; }
         public int Order { get; set; }
-
         public FakeCategory(int id)
         {
             Id = id;
